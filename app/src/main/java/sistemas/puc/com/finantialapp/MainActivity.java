2dc5ce028ecf1c185c817768964ad2b5b870b8d5
Tales Bontempo Cunha <tabocu@gmail.com>
2017-09-05 18:30:10 -0300

    Changing menu labels

--- app/src/main/java/sistemas/puc/com/finantialapp/MainActivity.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/MainActivity.java
@@ -73,17 +73,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_moeda) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_indice) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_tesouro) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_sobre) {
 
         }
 
