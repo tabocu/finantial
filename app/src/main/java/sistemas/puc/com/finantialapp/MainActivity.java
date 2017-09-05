440c402847804e381ff49636db98b0e8115c16ea
Tales Bontempo Cunha <tabocu@gmail.com>
2017-09-05 18:18:54 -0300

    Created fragment moeda

--- app/src/main/java/sistemas/puc/com/finantialapp/MainActivity.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/MainActivity.java
@@ -17,6 +17,11 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new MoedaFragment())
+                    .commit();
+        }
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
