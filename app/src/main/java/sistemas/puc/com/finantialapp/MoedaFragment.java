ed7c396ad6041f45e7660366b21d579d77eb7362
Tales Bontempo Cunha <tabocu@gmail.com>
2017-09-05 18:18:54 -0300

    Created fragment moeda

---
+++ app/src/main/java/sistemas/puc/com/finantialapp/MoedaFragment.java
@@ -0,0 +1,17 @@
+package sistemas.puc.com.finantialapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+public class MoedaFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_moeda, container, false);
+        return rootView;
+    }
+}
