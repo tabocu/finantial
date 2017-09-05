133bf1f17286253ad7ab11e086a81951b6896bbc
Tales Bontempo Cunha <tabocu@gmail.com>
2017-09-05 18:37:36 -0300

    Created indice fragment

---
+++ app/src/main/java/sistemas/puc/com/finantialapp/IndiceFragment.java
@@ -0,0 +1,17 @@
+package sistemas.puc.com.finantialapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class IndiceFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_indice, container, false);
+        return rootView;
+    }
+}
