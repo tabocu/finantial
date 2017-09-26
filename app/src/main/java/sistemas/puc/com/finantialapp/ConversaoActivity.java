25ff50fc4d7f609efac64ddde285f5fd2de45a23
Daniel Terenzi <danterenzi@hotmail.com>
2017-09-26 16:27:45 -0300

    Changes on conversao activity.

--- app/src/main/java/sistemas/puc/com/finantialapp/ConversaoActivity.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/ConversaoActivity.java
@@ -2,12 +2,23 @@ package sistemas.puc.com.finantialapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class ConversaoActivity extends AppCompatActivity {
+
+    EditText m_moedaEsqEditText;
+    EditText m_moedaDirEditText;
+    Spinner m_moedaEsqSpinner;
+    Spinner m_moedaDirSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversao);
+        m_moedaEsqEditText = (EditText) findViewById(R.id.moeda_esq_edittext);
+        m_moedaDirEditText = (EditText) findViewById(R.id.moeda_dir_edittext);
+        m_moedaEsqSpinner = (Spinner) findViewById(R.id.moeda_esq_spinner);
+        m_moedaDirSpinner = (Spinner) findViewById(R.id.moeda_dir_spinner);
     }
 }
