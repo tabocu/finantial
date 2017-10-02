c3a192c708e0c898efa90f16fd56a74ad351b508
Tales Bontempo Cunha <tabocu@gmail.com>
2017-10-02 16:11:56 -0300

    Refactoring database
    (cherry picked from commit ab0feb0)

--- app/src/main/java/sistemas/puc/com/finantialapp/TesouroFragment.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/TesouroFragment.java
@@ -1,6 +1,7 @@
 package sistemas.puc.com.finantialapp;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,7 @@ import java.util.List;
 
 import sistemas.puc.com.finantialapp.adapters.TesouroAdapter;
 import sistemas.puc.com.finantialapp.entities.TesouroItem;
+import sistemas.puc.com.finantialapp.model.Database;
 
 public class TesouroFragment extends Fragment {
 
@@ -21,30 +23,8 @@ public class TesouroFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TesouroItem[] data = {
-                new TesouroItem(
-                        "Tesouro IPCA+ 2024",
-                        "15/08/2024",
-                        "4,66","4,78",
-                        "2.194,90","2.177,62"),
-                new TesouroItem(
-                        "Tesouro IPCA+ 2035",
-                        "15/05/2035",
-                        "5,01","5,13",
-                        "1.270,65","1.245,36"),
-                new TesouroItem(
-                        "Tesouro IPCA+ 2045",
-                        "15/05/2045",
-                        "5,01","5,13",
-                        "780,38","756,19"),
-                new TesouroItem(
-                        "Tesouro SELIC 2023",
-                        "15/05/2045",
-                        "0,01","0,05",
-                        "9.077,87","9.058,13"),
-        };
 
-        List<TesouroItem> tesouroList = new ArrayList<>(Arrays.asList(data));
+        List<TesouroItem> tesouroList = Database.getTesouroList();
 
         m_tesouroAdapter = new TesouroAdapter(getActivity(), tesouroList);
 
--- app/src/main/java/sistemas/puc/com/finantialapp/model/Database.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/model/Database.java
@@ -4,19 +4,61 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import sistemas.puc.com.finantialapp.entities.IndiceItem;
 import sistemas.puc.com.finantialapp.entities.MoedaItem;
+import sistemas.puc.com.finantialapp.entities.TesouroItem;
 
-public class Database {
+public final class Database {
 
-    private static final MoedaItem[] moeda_data = {
+    private static final MoedaItem[] s_moedaData = {
             new MoedaItem("Dolar", "R$3,1133", "06/09/2017"),
             new MoedaItem("Libra", "R$4,0628", "06/09/2017"),
             new MoedaItem("Euro", "R$3,7145", "06/09/2017")
     };
 
+    private static final TesouroItem[] s_tesouroData = {
+            new TesouroItem(
+                    "Tesouro IPCA+ 2024",
+                    "15/08/2024",
+                    "4,66","4,78",
+                    "2.194,90","2.177,62"),
+            new TesouroItem(
+                    "Tesouro IPCA+ 2035",
+                    "15/05/2035",
+                    "5,01","5,13",
+                    "1.270,65","1.245,36"),
+            new TesouroItem(
+                    "Tesouro IPCA+ 2045",
+                    "15/05/2045",
+                    "5,01","5,13",
+                    "780,38","756,19"),
+            new TesouroItem(
+                    "Tesouro SELIC 2023",
+                    "15/05/2045",
+                    "0,01","0,05",
+                    "9.077,87","9.058,13"),
+    };
+
+    private static final IndiceItem[] s_indiceData = {
+            new IndiceItem.Simples("CDI", "11/09/2017", "8,14%"),
+            new IndiceItem.Simples("SELIC", "11/09/2017", "8,15%"),
+            new IndiceItem.Duplo("Poupança", "Setembro", "0,50%", "2017", "5,22%"),
+            new IndiceItem.Duplo("IPCA", "Agosto", "0,19%", "2017", "1,62%"),
+            new IndiceItem.Duplo("INPC", "Agosto", "-0,03%", "2017", "1,27%"),
+            new IndiceItem.Duplo("IGP-M", "Agosto", "0,10%", "2017", "-2,57%"),
+    };
+
     private Database() {}
 
     public static List<MoedaItem> getMoedaList() {
-        return new ArrayList<>(Arrays.asList(moeda_data));
+        return new ArrayList<>(Arrays.asList(s_moedaData));
+    }
+
+    public static List<TesouroItem> getTesouroList() {
+        return new ArrayList<>(Arrays.asList(s_tesouroData));
+    }
+
+    public static List<IndiceItem> getIndiceList() {
+        return new ArrayList<>(Arrays.asList(s_indiceData));
     }
 }
