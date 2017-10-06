e3df211ecefa8d29bc2b7dee77e0fe3255f82371
Tales Bontempo Cunha <tabocu@gmail.com>
2017-10-06 16:37:02 -0300

    Changing MoedaFragment from ListView to RecyclerView

--- app/src/main/java/sistemas/puc/com/finantialapp/MoedaFragment.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/MoedaFragment.java
@@ -3,6 +3,8 @@ package sistemas.puc.com.finantialapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
@@ -21,22 +23,24 @@ import sistemas.puc.com.finantialapp.model.Database;
 
 public class MoedaFragment extends Fragment {
 
-    private static final String DOLAR = "Dolar";
-    MoedaAdapter m_moedaAdapter;
+    private RecyclerView m_recyclerView;
+    private RecyclerView.Adapter m_adapter;
+    private RecyclerView.LayoutManager m_layoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        List<MoedaItem> moedaList = Database.getMoedaList();
-
-        m_moedaAdapter = new MoedaAdapter(getActivity(), moedaList);
-
         View rootView = inflater.inflate(R.layout.fragment_moeda, container, false);
 
-        ListView listView = (ListView) rootView.findViewById(R.id.listview_moeda);
-        listView.setAdapter(m_moedaAdapter);
-        registerForContextMenu(listView);
+        m_recyclerView = (RecyclerView) rootView.findViewById(R.id.listview_moeda);
+        m_recyclerView.setHasFixedSize(true);
+
+        m_layoutManager = new LinearLayoutManager(getContext());
+        m_recyclerView.setLayoutManager(m_layoutManager);
+
+        // get data set
+        List<MoedaItem> moedaList = Database.getMoedaList();
 
         return rootView;
     }
