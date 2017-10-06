adcd37ea4d5564901fbcd1d5ab53bc01e7256da7
Tales Bontempo Cunha <tabocu@gmail.com>
2017-10-06 12:27:42 -0300

    Removing key beacause a default key already exists

--- app/src/main/java/sistemas/puc/com/finantialapp/data/FinantialContract.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/data/FinantialContract.java
@@ -37,7 +37,6 @@ public class FinantialContract {
         public static final String TABLE_NAME = "moeda";
 
         // TODO: Add moeda columns definitions
-        public static final String COLUMN_MOEDA_KEY       = "moeda_id";
         public static final String COLUMN_MOEDA_INITIALS  = "moeda_initials";
         public static final String COLUMN_MOEDA_NAME      = "moeda_name";
         public static final String COLUMN_MOEDA_DATE      = "moeda_date";
