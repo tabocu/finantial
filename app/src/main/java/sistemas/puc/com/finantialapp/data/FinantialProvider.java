2748e0cd1b88fe0d8145c4ed732e80837f68732a
Tales Bontempo Cunha <tabocu@gmail.com>
2017-10-06 13:00:59 -0300

    Created URI matcher

--- app/src/main/java/sistemas/puc/com/finantialapp/data/FinantialProvider.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/data/FinantialProvider.java
@@ -1,7 +1,9 @@
 package sistemas.puc.com.finantialapp.data;
 
+import android.annotation.TargetApi;
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -9,9 +11,29 @@ import android.support.annotation.Nullable;
 
 public class FinantialProvider extends ContentProvider {
 
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private FinantialDbHelper mOpenHelper;
+
+    static final int MOEDA   = 100;
+    static final int TESOURO = 200;
+    static final int INDICE  = 300;
+
+    static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = FinantialContract.CONTENT_AUTHORITY;
+
+        // For each type of URI you want to add, create a corresponding code.
+        matcher.addURI(authority, FinantialContract.PATH_MOEDA, MOEDA);
+        matcher.addURI(authority, FinantialContract.PATH_TESOURO, TESOURO);
+        matcher.addURI(authority, FinantialContract.PATH_INDICE, INDICE);
+
+        return matcher;
+    }
+
     @Override
     public boolean onCreate() {
-        return false;
+        mOpenHelper = new FinantialDbHelper(getContext());
+        return true;
     }
 
     @Nullable
@@ -40,5 +62,12 @@ public class FinantialProvider extends ContentProvider {
     @Override
     public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
         return 0;
+    }
+
+    @Override
+    @TargetApi(11)
+    public void shutdown() {
+        mOpenHelper.close();
+        super.shutdown();
     }
 }
