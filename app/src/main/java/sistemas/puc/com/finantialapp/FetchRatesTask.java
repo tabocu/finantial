aca81194e9bf85a2f22b29072e24a79071f3d6f8
Daniel Terenzi <danterenzi@hotmail.com>
2017-10-06 15:31:49 -0300

    doInBackground implemented.

--- app/src/main/java/sistemas/puc/com/finantialapp/FetchRatesTask.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/FetchRatesTask.java
@@ -1,8 +1,19 @@
 package sistemas.puc.com.finantialapp;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class FetchRatesTask extends AsyncTask<Void, Void, Void>{
+
+    private static final String FIXER_URL = "http://api.fixer.io/latest?base=BRL";
+    private final String LOG_TAG = FetchRatesTask.class.getSimpleName();
 
     @Override
     protected void onPreExecute(){
@@ -11,6 +22,65 @@ public class FetchRatesTask extends AsyncTask<Void, Void, Void>{
 
     @Override
     protected Void doInBackground(Void... voids) {
+        // These two need to be declared outside the try/catch
+        // so that they can be closed in the finally block.
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        // Will contain the raw JSON response as a string.
+        String ratesJsonStr = null;
+
+        try {
+            // Construct the URL for the OpenWeatherMap query
+            // Possible parameters are avaiable at OWM's forecast API page, at
+            // http://openweathermap.org/API#forecast
+            URL url = new URL(FIXER_URL);
+
+            // Create the request to OpenWeatherMap, and open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+            ratesJsonStr = buffer.toString();
+            Log.d(LOG_TAG, ratesJsonStr);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error ", e);
+            // If the code didn't successfully get the weather data, there's no point in attemping
+            // to parse it.
+            return null;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(LOG_TAG, "Error closing stream", e);
+                }
+            }
+        }
         return null;
     }
 
