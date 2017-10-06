f5510fc32e5573b2b948288f4ae5f42b78d5741c
Tales Bontempo Cunha <tabocu@gmail.com>
2017-10-06 12:28:09 -0300

    Finished implementation of DbHelper for moeda

--- app/src/main/java/sistemas/puc/com/finantialapp/data/FinantialDbHelper.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/data/FinantialDbHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import sistemas.puc.com.finantialapp.data.FinantialContract.MoedaEntry;
+
 public class FinantialDbHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
@@ -16,11 +18,20 @@ public class FinantialDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_MOEDA_TABLE = "CREATE TABLE " + MoedaEntry.TABLE_NAME + " (" +
+                MoedaEntry._ID                   + " INTEGER PRIMARY KEY," +
+                MoedaEntry.COLUMN_MOEDA_INITIALS + " TEXT UNIQUE NOT NULL, " +
+                MoedaEntry.COLUMN_MOEDA_NAME     + " TEXT UNIQUE NOT NULL, " +
+                MoedaEntry.COLUMN_MOEDA_DATE     + " INTEGER NOT NULL, " +
+                MoedaEntry.COLUMN_MOEDA_RATE     + " REAL NOT NULL, " +
+                " );";
 
+        db.execSQL(SQL_CREATE_MOEDA_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + MoedaEntry.TABLE_NAME);
+        onCreate(db);
     }
 }
