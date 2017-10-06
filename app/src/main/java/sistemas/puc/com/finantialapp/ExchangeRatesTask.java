a76e7b45d7cf07dacf1290f9466fb06f429927b4
Daniel Terenzi <danterenzi@hotmail.com>
2017-10-06 09:56:51 -0300

    Created base asynctask with unimplemented methods.

---
+++ app/src/main/java/sistemas/puc/com/finantialapp/ExchangeRatesTask.java
@@ -0,0 +1,26 @@
+package sistemas.puc.com.finantialapp;
+
+import android.os.AsyncTask;
+
+public class ExchangeRatesTask extends AsyncTask<Void, Void, Void>{
+
+    @Override
+    protected void onPreExecute(){
+
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... voids) {
+
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+
+    }
+}
