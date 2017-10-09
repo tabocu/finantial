b01e5d6add03e45eb2d759e3c4fde20c4ee215f5
Tales Bontempo Cunha <tabocu@gmail.com>
2017-10-08 21:11:53 -0300

    Created density utils

---
+++ app/src/main/java/sistemas/puc/com/finantialapp/util/DensityUtils.java
@@ -0,0 +1,18 @@
+package sistemas.puc.com.finantialapp.util;
+
+import android.content.Context;
+
+public final class DensityUtils {
+
+    private DensityUtils() {}
+
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public static int px2dip(Context context, float pxValue) {
+        final float scale =  context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
