b1911d29ce09b21189f0b346f04808fbe405c2bb
Daniel Terenzi <danterenzi@hotmail.com>
2017-10-09 22:44:34 -0300

    Hashmap insertions.

--- app/src/main/java/sistemas/puc/com/finantialapp/model/MoedaMap.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/model/MoedaMap.java
@@ -9,7 +9,40 @@ public class MoedaMap {
 
     private static Map<String, String> initializeMap() {
         Map<String, String> result = new HashMap<String, String>();
-        result.put("USD", "Dolar");
+
+        result.put("AUD", "Dólar Australiano");
+        result.put("BGN", "Lev Búlgaro");
+        result.put("BRL", "Real");
+        result.put("CAD", "Dólar Canadense");
+        result.put("CHF", "Franco Suíço");
+        result.put("CNY", "Renminbi");
+        result.put("CZK", "Coroa Checa");
+        result.put("DKK", "Coroa Dinamarquesa");
+        result.put("EUR", "Euro");
+        result.put("GBP", "Libra Esterlina");
+        result.put("HKD", "Dólar de Hong Kong");
+        result.put("HRK", "Kuna Croata");
+        result.put("HUF", "Forint");
+        result.put("IDR", "Rúpia Indonésia");
+        result.put("ILS", "Shekel");
+        result.put("INR", "Rúpia Indiana");
+        result.put("JPY", "Iene");
+        result.put("KRW", "Won Sul-coreano");
+        result.put("MXN", "Peso Mexicano");
+        result.put("MYR", "Ringgit");
+        result.put("NOK", "Coroa Norueguesa");
+        result.put("NZD", "Dólar da Nóva Zelândia");
+        result.put("PHP", "Peso Filipino");
+        result.put("PLN", "Zloty");
+        result.put("RON", "Leu Romeno");
+        result.put("RUB", "Rublo");
+        result.put("SEK", "Coroa Sueca");
+        result.put("SGD", "Dólar de Singapura");
+        result.put("THB", "Baht");
+        result.put("TRY", "Nova Lira Turca");
+        result.put("USD", "Dólar");
+        result.put("ZAR", "Rand");
+
         return Collections.unmodifiableMap(result);
     }
 }
