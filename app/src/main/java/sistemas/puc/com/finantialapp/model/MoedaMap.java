d8d202b1f3fd7ca347874889a337f744386b8f07
Daniel Terenzi <danterenzi@hotmail.com>
2017-10-10 15:44:11 -0300

    Requested changes made.

--- app/src/main/java/sistemas/puc/com/finantialapp/model/MoedaMap.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/model/MoedaMap.java
@@ -1,14 +1,18 @@
 package sistemas.puc.com.finantialapp.model;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Size;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MoedaMap {
+public final class MoedaMap {
     private static final HashMap<String, String> MOEDA_MAP = new HashMap<>(initializeMap());
 
+    private MoedaMap() {}
+
+    // TODO: Move map to resources to enable translation.
     private static Map<String, String> initializeMap() {
         Map<String, String> result = new HashMap<String, String>();
 
@@ -52,7 +56,7 @@ public class MoedaMap {
      * @param code Currency code following ISO 4217.
      * @return     The currency name in portuguese.
      */
-    public static String getCurrencyName(@NonNull String code) {
+    public static String getCurrencyName(@NonNull @Size(3) String code) {
         return MOEDA_MAP.get(code);
     }
 }
