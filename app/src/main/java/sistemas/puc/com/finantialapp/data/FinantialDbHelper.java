081e818d7531bf49045c1fa23c84f04c622d8042
Tales Bontempo Cunha <tabocu@gmail.com>
2017-10-28 18:35:15 -0200

    Modified DB helper to include Indice

--- app/src/main/java/sistemas/puc/com/finantialapp/data/FinantialDbHelper.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/data/FinantialDbHelper.java
@@ -5,10 +5,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import sistemas.puc.com.finantialapp.data.FinantialContract.MoedaEntry;
+import sistemas.puc.com.finantialapp.data.FinantialContract.IndiceEntry;
 
 public class FinantialDbHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     static final String DATABASE_NAME = "finantial.db";
 
@@ -26,12 +27,24 @@ public class FinantialDbHelper extends SQLiteOpenHelper {
                 MoedaEntry.COLUMN_MOEDA_RATE + " REAL NOT NULL " +
                 " );";
 
+        final String SQL_CREATE_INDICE_TABLE = "CREATE TABLE " + IndiceEntry.TABLE_NAME + " (" +
+                IndiceEntry._ID                      + " INTEGER PRIMARY KEY," +
+                IndiceEntry.COLUMN_INDICE_CODE       + " TEXT UNIQUE NOT NULL, " +
+                IndiceEntry.COLUMN_INDICE_NAME       + " TEXT UNIQUE NOT NULL, " +
+                IndiceEntry.COLUMN_INDICE_DATE       + " INTEGER NOT NULL, " +
+                IndiceEntry.COLUMN_INDICE_MONTH_RATE + " REAL NOT NULL, " +
+                IndiceEntry.COLUMN_INDICE_YEAR_RATE  + " REAL NOT NULL, " +
+                IndiceEntry.COLUMN_INDICE_TYPE       + " INTEGER NOT NULL " +
+                " );";
+
         db.execSQL(SQL_CREATE_MOEDA_TABLE);
+        db.execSQL(SQL_CREATE_INDICE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + MoedaEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + IndiceEntry.TABLE_NAME);
         onCreate(db);
     }
 }
