704530a74469e698539a10c501ec916a2897dfe8
Daniel Terenzi <danterenzi@hotmail.com>
2017-11-06 20:58:36 -0200

    Base asyncTask for Selic/INPC/IPCA done.

--- app/src/main/java/sistemas/puc/com/finantialapp/FetchIndexTask.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/FetchIndexTask.java
@@ -30,15 +30,15 @@ public class FetchIndexTask extends AsyncTask<Void, Void, List<ContentValues>>{
 
     private final static String BCB_VALUE = "valor";
     private final static String BCB_DATE = "data";
-    private static final String SERIES_NUMBER = "{series_number}";
 
-    private static final String BCB_BASE_URL = "http://api.bcb.gov.br/dados/serie/bcdata.sgs."
-            +SERIES_NUMBER
-            +"/dados/ultimos/1?formato=json";
+    private final static String IBGE_VALUE = "v";
+    private final static String IBGE_DATE = "p_cod";
+    private final static int IBGE_MENSAL = 0;
+    private final static int IBGE_ANUAL = 1;
 
-    private static final String BCB_SELIC = "1178";
-
-    private static final String IPCA_COMERCIALIZAVEIS = "4447";
+    private static final String SELIC_URL = "http://api.bcb.gov.br/dados/serie/bcdata.sgs.1178/dados/ultimos/1?formato=json";
+    private static final String IPCA_URL = "http://servicodados.ibge.gov.br/api/v2/conjunturais/1419/periodos/-1/indicadores";
+    private static final String INPC_URL = "http://servicodados.ibge.gov.br/api/v2/conjunturais/1100/periodos/-1/indicadores";
 
     private final String LOG_TAG = FetchIndexTask.class.getSimpleName();
     private Context m_context;
@@ -53,58 +53,15 @@ public class FetchIndexTask extends AsyncTask<Void, Void, List<ContentValues>>{
     @Override
     protected List<ContentValues> doInBackground(Void... voids) {
 
-        HttpURLConnection urlConnection = null;
-        BufferedReader reader = null;
-
-        String selicJsonStr = null;
-
-        try {
-            String selicBaseUrl = BCB_BASE_URL.replace(SERIES_NUMBER, BCB_SELIC);
-
-            Uri selicBuiltUri = Uri.parse(selicBaseUrl).buildUpon().build();
-            URL selicUrl = new URL(selicBuiltUri.toString());
-
-            // Create the request and open the connection
-            urlConnection = (HttpURLConnection) selicUrl.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-
-            InputStream inputStream = urlConnection.getInputStream();
-            StringBuffer buffer = new StringBuffer();
-            if (inputStream == null) {
-                return null;
-            }
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                buffer.append(line + "\n");
-            }
-
-            if (buffer.length() != 0) {
-                selicJsonStr = buffer.toString();
-            } else {
-                return null;
-            }
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "Error ", e);
-            return null;
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (final IOException e) {
-                    Log.e(LOG_TAG, "Error closing stream", e);
-                }
-            }
-        }
+        String selicJsonStr = getJsonStr(SELIC_URL);
+        String inpcJsonStr = getJsonStr(INPC_URL);
+        String ipcaJsonStr = getJsonStr(IPCA_URL);
 
         try {
             List<ContentValues> result = new ArrayList<>();
             result.add(getSelicFromJson(selicJsonStr));
+            result.add(getIndiceFromIBGEJson(ipcaJsonStr, IndiceEnum.IPCA));
+            result.add(getIndiceFromIBGEJson(inpcJsonStr, IndiceEnum.INPC));
             return result;
         } catch (JSONException e) {
             Log.e(LOG_TAG, e.getMessage(), e);
@@ -126,30 +83,101 @@ public class FetchIndexTask extends AsyncTask<Void, Void, List<ContentValues>>{
         }
     }
 
+    private String getJsonStr(String URL) {
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        try {
+            Uri builtURI = Uri.parse(URL).buildUpon().build();
+            URL indexUrl = new URL(builtURI.toString());
+
+            // Create the request and open the connection
+            urlConnection = (HttpURLConnection) indexUrl.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuilder stringBuilder = new StringBuilder();
+            if (inputStream == null) {
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line + "\n");
+            }
+
+            if (stringBuilder.length() != 0) {
+                return stringBuilder.toString();
+            } else {
+                return null;
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error ", e);
+            return null;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(LOG_TAG, "Error closing stream", e);
+                }
+            }
+        }
+    }
+
     private ContentValues getSelicFromJson(String jsonStr) throws JSONException{
 
-
         ContentValues selic = new ContentValues();
+        setupIndiceCV(selic, IndiceEnum.SELIC);
 
         JSONArray jsonArray = new JSONArray(jsonStr);
 
         String date;
         long time;
         double value;
-        for (int i=0; i<jsonArray.length(); i++) {
-            JSONObject obj = jsonArray.getJSONObject(i);
 
-            date = obj.getString(BCB_DATE);
-            time = Util.getTimeFromDateString(date);
-            value = obj.getDouble(BCB_VALUE);
+        JSONObject obj = jsonArray.getJSONObject(jsonArray.length()-1);
 
-            setupIndiceCV(selic, IndiceEnum.SELIC);
-            selic.put(FinantialContract.IndiceEntry.COLUMN_INDICE_YEAR_RATE, value);
-            selic.put(FinantialContract.IndiceEntry.COLUMN_INDICE_DATE, time);
-            selic.put(FinantialContract.IndiceEntry.COLUMN_INDICE_MONTH_RATE, "??");
-        }
+        date = obj.getString(BCB_DATE);
+        time = Util.getTimeFromDateString(date);
+        value = obj.getDouble(BCB_VALUE);
+
+        selic.put(FinantialContract.IndiceEntry.COLUMN_INDICE_YEAR_RATE, value);
+        selic.put(FinantialContract.IndiceEntry.COLUMN_INDICE_DATE, time);
 
         return selic;
+    }
+
+    private ContentValues getIndiceFromIBGEJson(String jsonStr, IndiceEnum indiceEnum) throws JSONException {
+        ContentValues indice = new ContentValues();
+        setupIndiceCV(indice, indiceEnum);
+
+        JSONArray jsonArray = new JSONArray(jsonStr);
+
+        String date;
+        long time;
+        double value;
+
+        JSONObject obj = jsonArray.getJSONObject(IBGE_ANUAL);
+        date = obj.getString(IBGE_DATE);
+        time = Util.getTimeFromDateString(date);
+        value = obj.getDouble(IBGE_VALUE);
+
+        indice.put(FinantialContract.IndiceEntry.COLUMN_INDICE_YEAR_RATE, value);
+        indice.put(FinantialContract.IndiceEntry.COLUMN_INDICE_DATE, time);
+
+
+        obj = jsonArray.getJSONObject(IBGE_MENSAL);
+        value = obj.getDouble(IBGE_VALUE);
+        indice.put(FinantialContract.IndiceEntry.COLUMN_INDICE_MONTH_RATE, value);
+
+        return indice;
     }
 
     private void setupIndiceCV(ContentValues cv, IndiceEnum indiceEnum) {
