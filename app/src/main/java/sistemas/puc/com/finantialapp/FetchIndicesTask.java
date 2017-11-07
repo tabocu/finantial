3cd8a88a845a9445544783c370cd9eea5c3cf062
Daniel Terenzi <danterenzi@hotmail.com>
2017-11-07 21:38:09 -0200

    Requested changes made.

--- app/src/main/java/sistemas/puc/com/finantialapp/FetchIndicesTask.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/FetchIndicesTask.java
@@ -2,7 +2,6 @@ package sistemas.puc.com.finantialapp;
 
 import android.content.ContentValues;
 import android.content.Context;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -10,17 +9,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import sistemas.puc.com.finantialapp.data.FinantialContract;
 import sistemas.puc.com.finantialapp.model.IndiceEnum;
+import sistemas.puc.com.finantialapp.util.JsonUtils;
 import sistemas.puc.com.finantialapp.util.Util;
 
 public class FetchIndicesTask extends AsyncTask<Void, Void, List<ContentValues>>{
@@ -50,26 +44,23 @@ public class FetchIndicesTask extends AsyncTask<Void, Void, List<ContentValues>>
     @Override
     protected List<ContentValues> doInBackground(Void... voids) {
 
-        String selicJsonStr = getJsonStr(SELIC_URL);
-        String inpcJsonStr = getJsonStr(INPC_URL);
-        String ipcaJsonStr = getJsonStr(IPCA_URL);
+        String selicJsonStr = JsonUtils.getJsonStr(SELIC_URL);
+        String inpcJsonStr = JsonUtils.getJsonStr(INPC_URL);
+        String ipcaJsonStr = JsonUtils.getJsonStr(IPCA_URL);
 
         //placeholders
 
-        ContentValues CDI = new ContentValues();
-        setupIndiceCV(CDI, IndiceEnum.CDI);
+        ContentValues CDI = setupIndiceCV(IndiceEnum.CDI);
         CDI.put(FinantialContract.IndiceEntry.COLUMN_INDICE_MONTH_RATE, 0.0);
         CDI.put(FinantialContract.IndiceEntry.COLUMN_INDICE_YEAR_RATE, 0.0);
         CDI.put(FinantialContract.IndiceEntry.COLUMN_INDICE_DATE, "201709");
 
-        ContentValues poupanca = new ContentValues();
-        setupIndiceCV(poupanca, IndiceEnum.POUPANCA);
+        ContentValues poupanca = setupIndiceCV(IndiceEnum.POUPANCA);
         poupanca.put(FinantialContract.IndiceEntry.COLUMN_INDICE_MONTH_RATE, 0.0);
         poupanca.put(FinantialContract.IndiceEntry.COLUMN_INDICE_YEAR_RATE, 0.0);
         poupanca.put(FinantialContract.IndiceEntry.COLUMN_INDICE_DATE, "201709");
 
-        ContentValues IGPM = new ContentValues();
-        setupIndiceCV(IGPM, IndiceEnum.IGPM);
+        ContentValues IGPM = setupIndiceCV(IndiceEnum.IGPM);
         IGPM.put(FinantialContract.IndiceEntry.COLUMN_INDICE_MONTH_RATE, 0.0);
         IGPM.put(FinantialContract.IndiceEntry.COLUMN_INDICE_YEAR_RATE, 0.0);
         IGPM.put(FinantialContract.IndiceEntry.COLUMN_INDICE_DATE, "201709");
@@ -105,58 +96,10 @@ public class FetchIndicesTask extends AsyncTask<Void, Void, List<ContentValues>>
         }
     }
 
-    private String getJsonStr(String URL) {
-
-        HttpURLConnection urlConnection = null;
-        BufferedReader reader = null;
-
-        try {
-            Uri builtURI = Uri.parse(URL).buildUpon().build();
-            URL indexUrl = new URL(builtURI.toString());
-
-            // Create the request and open the connection
-            urlConnection = (HttpURLConnection) indexUrl.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-
-            InputStream inputStream = urlConnection.getInputStream();
-            StringBuilder stringBuilder = new StringBuilder();
-            if (inputStream == null) {
-                return null;
-            }
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                stringBuilder.append(line + "\n");
-            }
-
-            if (stringBuilder.length() != 0) {
-                return stringBuilder.toString();
-            } else {
-                return null;
-            }
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "Error ", e);
-            return null;
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (final IOException e) {
-                    Log.e(LOG_TAG, "Error closing stream", e);
-                }
-            }
-        }
-    }
 
     private ContentValues getSelicFromJson(String jsonStr) throws JSONException{
 
-        ContentValues selic = new ContentValues();
-        setupIndiceCV(selic, IndiceEnum.SELIC);
+        ContentValues selic = setupIndiceCV(IndiceEnum.SELIC);
 
         JSONArray jsonArray = new JSONArray(jsonStr);
 
@@ -179,8 +122,7 @@ public class FetchIndicesTask extends AsyncTask<Void, Void, List<ContentValues>>
 
     private ContentValues getIndiceFromIBGEJson(String jsonStr,
                                                 IndiceEnum indiceEnum) throws JSONException {
-        ContentValues indice = new ContentValues();
-        setupIndiceCV(indice, indiceEnum);
+        ContentValues indice = setupIndiceCV(indiceEnum);
 
         JSONArray jsonArray = new JSONArray(jsonStr);
 
@@ -204,9 +146,11 @@ public class FetchIndicesTask extends AsyncTask<Void, Void, List<ContentValues>>
         return indice;
     }
 
-    private void setupIndiceCV(ContentValues cv, IndiceEnum indiceEnum) {
+    private ContentValues setupIndiceCV(IndiceEnum indiceEnum) {
+        ContentValues cv = new ContentValues();
         cv.put(FinantialContract.IndiceEntry.COLUMN_INDICE_CODE, indiceEnum.getCode());
         cv.put(FinantialContract.IndiceEntry.COLUMN_INDICE_NAME, indiceEnum.getName());
         cv.put(FinantialContract.IndiceEntry.COLUMN_INDICE_TYPE, indiceEnum.getType().ordinal());
+        return cv;
     }
 }
