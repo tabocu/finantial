34188ddc0853fbcaf0ce904505799c4d9b4c722d
Tales Bontempo Cunha <tabocu@gmail.com>
2017-11-15 13:58:09 -0200

    Modified MoedaCursorAdapter to handle selection

--- app/src/main/java/sistemas/puc/com/finantialapp/adapters/MoedaCursorAdapter.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/adapters/MoedaCursorAdapter.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import sistemas.puc.com.finantialapp.R;
@@ -21,12 +22,16 @@ public class MoedaCursorAdapter extends AbstractCursorAdapter<MoedaCursorAdapter
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
+        public View view;
+        public View selectionPin;
         public TextView codigoMoeda;
         public TextView nomeMoeda;
         public TextView cotacaoMoeda;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            view = itemView.findViewById(R.id.item_moeda);
+            selectionPin = itemView.findViewById(R.id.selection_pin);
             codigoMoeda = (TextView) itemView.findViewById(R.id.item_moeda_code_textview);
             nomeMoeda = (TextView) itemView.findViewById(R.id.item_moeda_name_textview);
             cotacaoMoeda = (TextView) itemView.findViewById(R.id.item_moeda_value_textview);
@@ -42,9 +47,13 @@ public class MoedaCursorAdapter extends AbstractCursorAdapter<MoedaCursorAdapter
 
     @Override
     public void onBindViewHolder(ViewHolder holder, Cursor cursor) {
+        int columnMoedaId   = cursor.getColumnIndexOrThrow(MoedaEntry._ID);
         int columnMoedaCode = cursor.getColumnIndexOrThrow(MoedaEntry.COLUMN_MOEDA_CODE);
         int columnMoedaName = cursor.getColumnIndexOrThrow(MoedaEntry.COLUMN_MOEDA_NAME);
         int columnMoedaRate = cursor.getColumnIndexOrThrow(MoedaEntry.COLUMN_MOEDA_RATE);
+
+        holder.selectionPin.setVisibility(isItemSelected(cursor.getInt(columnMoedaId)) ? View.VISIBLE : View.INVISIBLE);
+        holder.view.setSelected(isItemSelected(cursor.getInt(columnMoedaId)));
 
         holder.codigoMoeda.setText(cursor.getString(columnMoedaCode));
         holder.nomeMoeda.setText(cursor.getString(columnMoedaName));
