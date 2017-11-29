29aad00a31021b5ce74f6ad86b84c70c077394e4
Tales Bontempo Cunha <tabocu@gmail.com>
2017-11-29 17:56:48 -0200

    Refactor TesouroFragment

--- app/src/main/java/sistemas/puc/com/finantialapp/tesouro/TesouroFragment.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/tesouro/TesouroFragment.java
@@ -1,12 +1,8 @@
 package sistemas.puc.com.finantialapp.tesouro;
 
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -17,24 +13,8 @@ import sistemas.puc.com.finantialapp.R;
 import sistemas.puc.com.finantialapp.adapters.AbstractCursorAdapter;
 import sistemas.puc.com.finantialapp.model.Database;
 import sistemas.puc.com.finantialapp.util.DividerItemDecoration;
-import sistemas.puc.com.finantialapp.data.FinantialContract.TesouroEntry;
 
-public class TesouroFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
-
-    private static final Uri TESOURO_URI = TesouroEntry.CONTENT_URI;
-
-    private static final String[] TESOURO_COLUMNS = new String[]{
-            TesouroEntry.TABLE_NAME + "." + TesouroEntry._ID,
-            TesouroEntry.TABLE_NAME + "." + TesouroEntry.COLUMN_TESOURO_NAME,
-            TesouroEntry.TABLE_NAME + "." + TesouroEntry.COLUMN_TESOURO_MODE,
-            TesouroEntry.TABLE_NAME + "." + TesouroEntry.COLUMN_TESOURO_YEAR,
-            TesouroEntry.TABLE_NAME + "." + TesouroEntry.COLUMN_TESOURO_EXPIRATION_DATE,
-            TesouroEntry.TABLE_NAME + "." + TesouroEntry.COLUMN_TESOURO_BUYING_INCOME,
-            TesouroEntry.TABLE_NAME + "." + TesouroEntry.COLUMN_TESOURO_SELLING_INCOME,
-            TesouroEntry.TABLE_NAME + "." + TesouroEntry.COLUMN_TESOURO_BUYING_PRICE,
-            TesouroEntry.TABLE_NAME + "." + TesouroEntry.COLUMN_TESOURO_BUYING_MIN_VALUE,
-            TesouroEntry.TABLE_NAME + "." + TesouroEntry.COLUMN_TESOURO_SELLING_PRICE,
-    };
+public class TesouroFragment extends Fragment {
 
     private RecyclerView m_recyclerView;
     private AbstractCursorAdapter m_adapter;
@@ -43,51 +23,35 @@ public class TesouroFragment extends Fragment implements LoaderManager.LoaderCal
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.fragment_tesouro, container, false);
 
         // Update database on startup
         Database.update(getContext());
-
-        DividerItemDecoration itemDecoration = new DividerItemDecoration(getActivity());
-        itemDecoration.setLastItemIncluded(false);
-        itemDecoration.setPaddingLeftDp(32);
-        itemDecoration.setPaddingRightDp(32);
+        
+        m_adapter = new TesouroCursorAdapter(getContext());
+        m_layoutManager = new LinearLayoutManager(getContext());
 
         m_recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerview_tesouro);
-        m_recyclerView.addItemDecoration(itemDecoration);
-        m_recyclerView.setHasFixedSize(true);
-
-        m_layoutManager = new LinearLayoutManager(getContext());
-        m_recyclerView.setLayoutManager(m_layoutManager);
-
-        m_adapter = new TesouroCursorAdapter(getContext());
-
         m_recyclerView.setAdapter(m_adapter);
+        m_recyclerView.setLayoutManager(m_layoutManager);
+        m_recyclerView.setHasFixedSize(true);
+        m_recyclerView.addItemDecoration(getItemDecoration());
 
-        getLoaderManager().initLoader(0, null, this);
+        getLoaderManager().initLoader(
+                0,
+                null,
+                new TesouroLoaderCallback(getActivity(), m_adapter));
 
         return rootView;
     }
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return new CursorLoader(
-                getActivity(),
-                TESOURO_URI,
-                TESOURO_COLUMNS,
-                null,
-                null,
-                null);
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        m_adapter.swapCursor(data);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-        m_adapter.swapCursor(null);
+    @NonNull
+    private DividerItemDecoration getItemDecoration() {
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(getActivity());
+        itemDecoration.setLastItemIncluded(false);
+        // TODO: Fix hardcoded numbers
+        itemDecoration.setPaddingLeftDp(32);
+        itemDecoration.setPaddingRightDp(32);
+        return itemDecoration;
     }
 }
