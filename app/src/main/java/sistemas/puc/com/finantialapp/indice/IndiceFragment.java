ab9293a6284198c485b24489e4c6ac4c18fa4890
Tales Bontempo Cunha <tabocu@gmail.com>
2017-12-10 21:01:23 -0200

    Refactor IndiceFragment

--- app/src/main/java/sistemas/puc/com/finantialapp/indice/IndiceFragment.java
+++ app/src/main/java/sistemas/puc/com/finantialapp/indice/IndiceFragment.java
@@ -1,6 +1,7 @@
 package sistemas.puc.com.finantialapp.indice;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,40 +16,42 @@ import sistemas.puc.com.finantialapp.util.DividerItemDecoration;
 
 public class IndiceFragment extends Fragment {
 
-    private RecyclerView m_recyclerView;
-    private AbstractCursorAdapter m_adapter;
-    private RecyclerView.LayoutManager m_layoutManager;
+    private RecyclerView mRecyclerView;
+    private AbstractCursorAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.fragment_indice, container, false);
 
         // Update database on startup
         Database.update(getContext());
 
-        DividerItemDecoration itemDecoration = new DividerItemDecoration(getActivity());
-        itemDecoration.setLastItemIncluded(false);
-        itemDecoration.setPaddingLeftDp(32);
-        itemDecoration.setPaddingRightDp(32);
+        mAdapter = new IndiceCursorAdapter(getContext());
+        mLayoutManager = new LinearLayoutManager(getContext());
 
-        m_recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerview_indice);
-        m_recyclerView.addItemDecoration(itemDecoration);
-        m_recyclerView.setHasFixedSize(true);
-
-        m_layoutManager = new LinearLayoutManager(getContext());
-        m_recyclerView.setLayoutManager(m_layoutManager);
-
-        m_adapter = new IndiceCursorAdapter(getContext(), null);
-
-        m_recyclerView.setAdapter(m_adapter);
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerview_indice);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.addItemDecoration(getItemDecoration());
 
         getLoaderManager().initLoader(
                 0,
                 null,
-                new IndiceLoaderCallback(getActivity(), m_adapter));
+                new IndiceLoaderCallback(getActivity(), mAdapter));
 
         return rootView;
+    }
+
+    @NonNull
+    private DividerItemDecoration getItemDecoration() {
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(getActivity());
+        itemDecoration.setLastItemIncluded(false);
+        // TODO: Fix hardcoded numbers
+        itemDecoration.setPaddingLeftDp(32);
+        itemDecoration.setPaddingRightDp(32);
+        return itemDecoration;
     }
 }
